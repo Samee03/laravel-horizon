a184dae4a5a1c575490bc253c895061453b60f7c
Samee <sameeullah03@gmail.com>
2022-12-30 20:18:19 +0500

    - Horizon implemented!

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use App\Notifications\NotifyUser;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('send', function () {
+    User::all()->map->notify(new NotifyUser());
+    return 'Done';
 });
